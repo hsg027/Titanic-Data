a78a35b0d07da37c47cdea7e66b98fdc159bab78
harshitz4u27 <55305677+hsg027@users.noreply.github.com>
2020-01-31 19:33:15 -0500

    Update DataPreprocessing.py

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -102,3 +102,63 @@ responses=pd.Categorical(combined_data.PassengerId, ordered=True)
 df_responses = pd.DataFrame({"response": responses})
 print("Passenger ID data analysis: ")
 print(df_responses.astype(str).describe())
+
+#11
+
+histogram_1= sns.FacetGrid(train_data, col ='Survived', size=6)
+histogram_1.map(plt.hist, 'Age', bins=range(0, 130, 5))
+plt.xticks(range(0, 130, 5))
+
+#12
+
+histogram_2 = sns.FacetGrid(train_data, col='Survived', row='Pclass', size=3, aspect=1.5)
+histogram_2.map(plt.hist, 'Age', bins=20)
+histogram_2.add_legend();
+
+#13
+
+histogram_3 = sns.FacetGrid(train_data, row='Embarked', col='Survived', height=3, aspect=1.5)
+histogram_3.map(sns.barplot, 'Sex', 'Fare')
+histogram_3.add_legend()
+
+#14
+train_data['Ticket'].describe()
+duplicacy_rate = ((891-681)/891)*100
+print(duplicacy_rate)
+
+#15
+
+combined_data.describe(include=["O"])
+
+#16
+
+sex = {'male' :0, 'female':1}
+combined_data['Gender'] = combined_data['Sex'].map(sex)
+combined_data.head()
+
+#17
+combined_data['Age'].fillna(np.random.uniform(low=combined_data['Age'].std(), high=combined_data['Age'].mean()),inplace=True)
+combined_data['Age'].isna().sum()
+
+#18
+combined_data['Embarked'].isna().sum()
+combined_data['Embarked'] = combined_data['Embarked'].fillna(train_data.Embarked.describe().top)
+combined_data['Embarked'].isna().sum()
+
+#19
+combined_data['Fare'].fillna(combined_data['Fare'].mode()[0],inplace =True)
+combined_data['Fare'].isna().sum()
+
+#20
+combined_data['FareBand'] = pd.qcut(combined_data['Fare'], 4)
+val = combined_data.FareBand.unique().get_values()
+val.sort()
+for i in range(len(val)):
+    combined_data.loc[(combined_data['Fare'] > val[i].left) & (combined_data['Fare'] <= val[i].right), 'Fare'] = i
+combined_data['Fare'] = combined_data['Fare'].astype(int)
+
+combined_data.head(10)
+
+
+#RUN EACH LINE OF CODE ONE BY ONE
+
